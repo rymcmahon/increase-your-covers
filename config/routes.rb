b63c4c01184e09956a50e1906a2cdf46f1c43d02
rymcmahon <toedragdangle9@gmail.com>
2016-12-20 22:27:53 -0600

    built out new assessment request form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
-  get '/website_critic' => 'pages#website_critic'
   resources :articles
+  resources :assessment_requests, only: [:new, :create], path: 'website_critic'
 end
