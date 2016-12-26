681ab74b1f0fbc21c84cebab8f9f5ff8cea32b54
rymcmahon <toedragdangle9@gmail.com>
2016-12-25 22:14:24 -0600

    added paperclip, aws sdk, and got image to upload to s3

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
   validates :title, :body, presence: true
+  has_attached_file :image, styles: { large: "800x800>"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
