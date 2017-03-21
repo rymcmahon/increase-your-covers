4305e1f3f3a0542f58d1d84e9e05b78df1134e03
rymcmahon <toedragdangle9@gmail.com>
2017-03-21 14:26:07 -0500

    removed objections from homepage and added services page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   resources :articles
-  resources :assessment_requests, only: [:new, :create], path: 'website_critic'
+  # resources :assessment_requests, only: [:new, :create], path: 'website_critic'
+  get 'pages/services' => 'pages#services'
 end
