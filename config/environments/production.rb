885f03d8671c6440a7733f13338b4c06bb43627c
rymcmahon <toedragdangle9@gmail.com>
2017-04-11 13:01:52 -0500

    added paperclip config variables to config prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,13 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+    bucket: ENV['AWS_S3_BUCKET'],
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
